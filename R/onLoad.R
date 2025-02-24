0c3b220efe206c3b7cd7df84ca515a11bd456aa5
Yanpeng Chen <chenyanpeng1992@outlook.com>
2025-02-24 13:12:05 +0800

    Update onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -2,7 +2,6 @@
 .onLoad <- function(libname, pkgname) {
   # Load outline
   # Get outline.xlsx from the fungioutline package
-  data_path <- list.files(system.file("data", package = "fungioutline"), pattern = "\\.xlsx$", full.names = TRUE)
-  xlsx_file <- file.path(data_path, "outline_2025.2.20.xlsx")
+  xlsx_file <- file.path(system.file("data", package = "fungioutline"), "outline_2025.2.20.xlsx")
   outline <<- readxl::read_excel(xlsx_file)
 }
