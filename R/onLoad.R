efe2bb34f70e1bc6b159c17b58ca5919532de353
Yanpeng Chen <chenyanpeng1992@outlook.com>
2025-03-15 12:15:39 +0800

    Update onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -5,8 +5,8 @@
   # Get outline.xlsx from the fungioutline package
   xlsx_file <- list.files(system.file("data", package = "fungioutline"),
                             pattern = "^outline.*xlsx")
-  outline <<- readxl::read_excel(xlsx_file)
-  
+  outline <- readxl::read_excel(system.file("data", package = "fungioutline", xlsx_file)) %>%
+      select(Kingdom, Subkingdoms, Phyla, Subphyla, Classes, Subclasses, Orders, Families, Genera)
   red_bold <- "\033[1;31m"  
   reset <- "\033[0m"       
   cat(red_bold, "Last update: March 15, 2025", reset, "\n")
