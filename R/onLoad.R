e25298e344fd4c6254b8f6c3ed7ed7dc2f1c476e
Yanpeng Chen <chenyanpeng1992@outlook.com>
2025-03-15 11:58:04 +0800

    updates

--- R/onLoad.R
+++ R/onLoad.R
@@ -3,6 +3,11 @@
   library(tidyverse)
   # Load outline
   # Get outline.xlsx from the fungioutline package
-  xlsx_file <- file.path(system.file("data", package = "fungioutline"), "outline_2025.2.20.xlsx")
+  xlsx_file <- list.files(system.file("data", package = "fungioutline"),
+                            pattern = "^outline.*xlsx")
   outline <<- readxl::read_excel(xlsx_file)
+  
+  red_bold <- "\033[1;31m"  
+  reset <- "\033[0m"       
+  cat(red_bold, "Last update: March 15, 2025", reset, "\n")
 }
