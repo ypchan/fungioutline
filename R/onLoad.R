7309b078c637b6a467a4ecaffd4681e358afee3a
Yanpeng Chen <chenyanpeng1992@outlook.com>
2025-03-15 12:28:17 +0800

    1

--- R/onLoad.R
+++ R/onLoad.R
@@ -5,7 +5,7 @@
   # Get outline.xlsx from the fungioutline package
   xlsx_file <- list.files(system.file("data", package = "fungioutline"),
                             pattern = "^outline.*xlsx")
-  outline <- readxl::read_excel(system.file("data", package = "fungioutline", xlsx_file)) %>%
+  outline <<- readxl::read_excel(system.file("data", package = "fungioutline", xlsx_file)) %>%
       select(Kingdom, Subkingdoms, Phyla, Subphyla, Classes, Subclasses, Orders, Families, Genera)
   red_bold <- "\033[1;31m"  
   reset <- "\033[0m"       
