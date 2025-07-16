cd849b964a8a2d052e4cb48a9b2e4cd2bc04fa16
Yanpeng Chen <chenyanpeng1992@outlook.com>
2025-07-16 14:23:38 +0800

    update outline header line

--- R/onLoad.R
+++ R/onLoad.R
@@ -5,12 +5,9 @@
   # Get outline.xlsx from the fungioutline package
   xlsx_file <- list.files(system.file("data", package = "fungioutline"),
                             pattern = "^outline.*xlsx")
-  red_bold <- "\033[1;31m"  
-  reset <- "\033[0m"       
-  cat("\n", red_bold, "Updated fungioutline: March 15, 2025", reset, "\n")
   outline <- readxl::read_excel(system.file("data", package = "fungioutline", xlsx_file), 
                                 col_types = rep("text", 21)) %>%
-      select(Kingdom, Subkingdoms, Phyla, Subphyla, Classes, Subclasses, Orders, Families, Genera) 
+      select(Kingdom, Subkingdom, Phylum, Subphylum, Class, Subclass, Order, Family, Genus) 
   assign("outline", outline, envir = .GlobalEnv)
 }
 #detach()
